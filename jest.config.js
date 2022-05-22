934d3165e2ec8f3a44873a642cb8aef91ea229a1
Jonathan Alves de Lima <jlima.dev@gmail.com>
2022-05-22 17:04:26 -0300

    chore(config): update jest cconfig

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,21 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  modulePathIgnorePatterns: ["<rootDir>/dist/"]
+  modulePathIgnorePatterns: ['<rootDir>/coverage/'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/coverage/', '<rootDir>/dist/'],
+  testMatch: ['<rootDir>/**/*.spec.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/coverage/', '<rootDir>/dist/'],
+  watchPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/coverage/', '<rootDir>/dist/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.ts', '!**/cli.ts'],
+  coverageDirectory: './coverage',
+  coverageReporters: ['lcov', 'text'],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
 };
