41c7d044e11030a15ff0374c48b8ba62c075f49a
Jonathan Alves de Lima <jlima.dev@gmail.com>
2022-05-22 18:06:19 -0300

    test(KinesisClientAdapter): create test cases to adapter

---
+++ src/test/mocks.ts
@@ -0,0 +1,57 @@
+import { PutRecordsRequestEntry } from '@aws-sdk/client-kinesis';
+import { JSONObject } from '../core/domain/JSONObject';
+
+export const streamProps = {
+  partitionKey: 'any-partition-key',
+  streamName: 'any-stream-name',
+  endpoint: 'http://any-enpoint:4566',
+  operation: 'load',
+  recordFileDir: 'fileDir',
+  chunkSize: '10',
+};
+export const firstJsonPayload: JSONObject = {
+  id: 1,
+  name: 'Joselito Naruto',
+  age: 18,
+};
+export const secondJsonPayload: JSONObject = {
+  id: 1,
+  name: 'Nagas Bike',
+  age: 25,
+};
+export const mockJsonPayloads = [firstJsonPayload, secondJsonPayload];
+export const mockPutRecordsRequestEntry: PutRecordsRequestEntry[] = [
+  {
+    Data: Buffer.from(JSON.stringify(firstJsonPayload)),
+    PartitionKey: streamProps.partitionKey,
+  },
+  {
+    Data: Buffer.from(JSON.stringify(secondJsonPayload)),
+    PartitionKey: streamProps.partitionKey,
+  },
+];
+
+export const mockDMSPayload = [
+  {
+    data: { age: 18, id: 1, name: 'Joselito Naruto' },
+    metadata: {
+      operation: 'load',
+      'partition-key-type': 'primary-key',
+      'record-type': 'data',
+      'schema-name': 'schema1',
+      'table-name': 'table1',
+      timestamp: '2022-05-22T10:00:00.0000Z',
+    },
+  },
+  {
+    data: { age: 25, id: 1, name: 'Nagas Bike' },
+    metadata: {
+      operation: 'load',
+      'partition-key-type': 'primary-key',
+      'record-type': 'data',
+      'schema-name': 'schema1',
+      'table-name': 'table1',
+      timestamp: '2022-05-22T10:00:00.0000Z',
+    },
+  },
+];
